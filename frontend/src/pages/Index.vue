4f848d5e20ba2e34d10fe54c27aa2f3a365caf13
Broodco <broodcoorens.maxime@gmail.com>
2019-07-12 16:55:21 +0200

    add work in progress text

--- frontend/src/pages/Index.vue
+++ frontend/src/pages/Index.vue
@@ -1,5 +1,8 @@
 <template>
-    <q-page class="flex flex-center">
+    <q-page class="flex flex-center text-center q-pa">
+        <h3>
+            Hello, this is a <i>work in progress</i>, come back later!
+        </h3>
     </q-page>
 </template>
 
